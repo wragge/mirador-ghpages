464c369914c9da838be6b02e338d8b59ffd401d8
Tim Sherratt <tim@discontents.com.au>
2024-05-31 23:30:16 +1000

    add plugin

--- add_manifests.py
+++ add_manifests.py
@@ -3,8 +3,10 @@ import re
 
 js = Path("src/index.js").read_text()
 mfs = []
+if match := re.search(r"windows: \[(.*?)\]", js):
+    mfs.append(match.group(1))
 for mf in Path("static", "manifests").glob("*.json"):
     mfs.append(f"{{manifestId: '../manifests/{mf.name}'}}")
-mf_str = rf"windows: [{', '.join(mfs)}]"
-js = re.sub(r"windows: \[.*\]", mf_str, js, flags=re.DOTALL)
+mf_str = rf"windows: [{', '.join([m for m in mfs if m])}]"
+js = re.sub(r"windows: \[.*?\]", mf_str, js, flags=re.DOTALL)
 Path("src/index.js").write_text(js)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import Mirador from 'mirador/dist/es/src/index';
+import { miradorImageToolsPlugin } from 'mirador-image-tools';
+//import annotationPlugins from 'mirador-annotations';
+//import LocalStorageAdapter from 'mirador-annotations/es/LocalStorageAdapter';
 
 const config = {
+    annotation: {
+        adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
+        // adapter: (canvasId) => new AnnototAdapter(canvasId, endpointUrl),
+        exportLocalStorageAnnotations: true, // display annotation JSON export button
+    },
     id: 'demo',
-    windows: [{manifestId: '../manifests/nla.obj-224441858-v3-manifest.json'}, {manifestId: '../manifests/nla.obj-224441684-v3-manifest.json'}],
+    windows: [],
     workspace: {
         type: "elastic"
     }
 };
 
-Mirador.viewer(config);
+Mirador.viewer(config, [
+    ...miradorImageToolsPlugin,
+    //...annotationPlugins
+]);
