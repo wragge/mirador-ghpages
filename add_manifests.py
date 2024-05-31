7cd7d9c9318b5deaa39ef428557bff014c4e6094
Tim Sherratt <tim@discontents.com.au>
2024-05-31 23:48:25 +1000

    change mf path

--- add_manifests.py
+++ add_manifests.py
@@ -6,7 +6,7 @@ mfs = []
 if match := re.search(r"windows: \[(.*?)\]", js):
     mfs.append(match.group(1))
 for mf in Path("static", "manifests").glob("*.json"):
-    mfs.append(f"{{manifestId: '../manifests/{mf.name}'}}")
+    mfs.append(f"{{manifestId: 'manifests/{mf.name}'}}")
 mf_str = rf"windows: [{', '.join([m for m in mfs if m])}]"
 js = re.sub(r"windows: \[.*?\]", mf_str, js, flags=re.DOTALL)
 Path("src/index.js").write_text(js)
